8a1bb293ad2b4a691f327a27b664d4e2e9d616fb
Mustafaelhdad <76115086+Mustafaelhdad@users.noreply.github.com>
2025-01-14 00:28:15 +0200

    ...

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+<div>Hello from the blade template view</div>
+
+
+@isset($name)
+    <h2>
+        My name is: {{ $name }}
+    </h2>
+@endisset
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index', ['name' => 'Mustafa']);
+});
+
+Route::get('/hello', function () {
+    return 'hello route';
+});
+
+Route::get('/greet/{name}', function ($name) { return 'Hello ' . $name . '!'; })->name('greet');
+
+Route::fallback(function () {
+    return '<h1 class="text-center">Not found</h1>';
 });
